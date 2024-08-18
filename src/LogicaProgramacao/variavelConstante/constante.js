cbece76aaf38c0d027b74c4ff6bf26d63a216f04
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-08-18 16:49:21 -0300

    LogicaProgramacao

--- src/LogicaProgramacao/variavelConstante/constante.js
+++
@@ -1,8 +0,0 @@
-const notificacao = "Pokemon Go diz: " //constante notificacao (imutável)
-
-console.log(notificacao + "tem um novo pokemon na região.")
-console.log(notificacao + "você foi derrotado por um Líder.")
-
-notificacao = "Digimon Go diz: "
-
-console.logo("o nome do jogo agora é: " + notificacao)
--- src/LogicaProgramacao/variavelConstante/variavel.js
+++
@@ -1,6 +0,0 @@
-console.log("Digita o nome do seu jogador") //saída
-
-let jogador = "Paulo mestre dos magos." //declarando a variável jogador
-
-console.log("Bem vindo jogador " + jogador) //concatenando uma mensagem fixa + uma variável
-console.log(jogador + " entrou no servidor")
