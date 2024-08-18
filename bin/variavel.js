e27c7e1cc3f0fe3f615e54c0fd983126aaf222b9
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-08-17 21:00:28 -0300

    LogicaProgramacao

---
+++ bin/variavel.js
@@ -0,0 +1,8 @@
+const notificacao = "Pokemon Go diz: " //constante notificacao (imutável)
+
+console.log(notificacao + "tem um novo pokemon na região.")
+console.log(notificação + "você foi derrotado por um Líder.")
+
+notificacao = "Digimon Go diz: "
+
+console.logo("o nome do jogo agora é: " + notificacao)
