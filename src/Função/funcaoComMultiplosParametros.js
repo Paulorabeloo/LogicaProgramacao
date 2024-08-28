a8ba95c15d3d6ffd2bfe26f12ffe620360a14a23
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-08-27 21:39:44 -0300

    LogicaProgramacao

---
+++ src/Função/funcaoComMultiplosParametros.js
@@ -0,0 +1,8 @@
+// função com mais de um parâmetro
+function torrar(pao, nome)
+{
+    console.log("torrada feita com " + pao)
+    console.log("ela é um pedido de " + nome)
+}
+
+torrar("pão de forma", "felipe")
