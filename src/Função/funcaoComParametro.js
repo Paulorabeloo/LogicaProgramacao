47768d944b3af31afa18afbce570b597e4147e33
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-08-27 21:23:11 -0300

    LogicaProgramacao

---
+++ src/Função/funcaoComParametro.js
@@ -0,0 +1,8 @@
+function torrar(pao){
+	console.log("torrada feita com " + pao)
+}
+
+torrar("pão de forma") //chamei a função torrar e passei como parâmetro o texto e vai guardar dentro da variável pao da função torrar
+torrar("pão integral")
+
+//geralmente se passa parâmetros quando a gente quer que uma mesma função tenha resultados diferentes
